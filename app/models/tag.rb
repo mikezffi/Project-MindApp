88be91788a1af4a0c71cfafc1ce8472ccf1069ec
mikezffi <mikeaazuffi@gmail.com>
2021-04-22 12:46:07 -0300

    Permite adicionar tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: { scope: user_id }
+  validates :name, presence: true #uniqueness: { scope: user_id }
 
   has_many :tag_contents, dependent: :destroy
   has_many :contents, through: :tag_contents
